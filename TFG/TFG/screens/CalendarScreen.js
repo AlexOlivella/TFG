d578169bbc73bb1499bd796eed5a92e83b9c7209
Alex31397 <alex.olivella@est.fib.upc.edu>
2019-12-23 13:04:04 +0100

    Veure appointments + migraines

--- TFG/TFG/screens/CalendarScreen.js
+++ TFG/TFG/screens/CalendarScreen.js
@@ -7,6 +7,7 @@ import * as FirebaseAPI from '../modules/firebaseAPI'
 import { Header, Icon, SearchBar, List, ListItem, } from 'react-native-elements'
 import { ScrollView } from 'react-native-gesture-handler';
 import Swipeable from 'react-native-gesture-handler/Swipeable';
+import RadioForm, { RadioButton, RadioButtonInput, RadioButtonLabel } from 'react-native-simple-radio-button';
 
 
 export default class prova extends Component {
@@ -22,6 +23,7 @@ export default class prova extends Component {
       markedDays: [],
       isLoaded: true,
       tipus: "",
+      value: ""
     }
     this.arrayHolder = [];
   }
@@ -87,7 +89,7 @@ export default class prova extends Component {
       var date = data.getDate(); //Current Date
       var month = data.getMonth() + 1; //Current Month
       var year = data.getFullYear(); //Current Year
-      var hour= data.getHours(); //Current Hours
+      var hour = data.getHours(); //Current Hours
       var min = data.getMinutes(); //Current Minutes
       var sec = data.getSeconds(); //Current Seconds
       if (min < 10) {
@@ -96,7 +98,7 @@ export default class prova extends Component {
       if (hour < 10) {
         hour = '0' + hour;
       }
-      return date + '-' + month + '-' + year + ' ' + hour+ ':' + min
+      return date + '-' + month + '-' + year + ' ' + hour + ':' + min
     }
     else return ""
   }
@@ -113,36 +115,6 @@ export default class prova extends Component {
     this.props.navigation.navigate("InfoMigranyesCalendari", { pacient: user.uid, migranya: migranya_id })
   }
 
-  renderRightComponent = (progress, dragX) => {
-    const trans = dragX.interpolate({
-      inputRange: [0, 50, 100, 101],
-      outputRange: [-20, 0, 0, 1],
-    });
-    var carregar
-    if (!this.state.isLoaded) carregar = <View><ActivityIndicator size="large"></ActivityIndicator></View>
-    return (
-      <View style={{flex:1}}>
-        <View style={{ justifyContent: 'center', alignItems: 'center' }}><Text style={{ fontSize: 20 }}>Migraines list</Text></View>
-        {carregar}
-
-        <FlatList
-          data={this.state.llistaMigranyes}
-          renderItem={({ item }) =>
-            <TouchableOpacity>
-              <ListItem containerStyle={{ backgroundColor: "#7BF0E6", borderBottomWidth: 1, borderBottomColor: 'white' }}
-                title={this.transformaData(item)}
-              />
-            </TouchableOpacity>
-          }
-          ListFooterComponent={this.renderFooter}
-          ItemSeparatorComponent={this.renderSeparator}
-
-          keyExtractor={item => item}
-        />
-      </View>
-    );
-  };
-
   seeDetailsAppointment(data_appointment) {
     this.props.navigation.navigate("AppointmentDetails", { day: data_appointment })
   }
@@ -156,9 +128,64 @@ export default class prova extends Component {
     const vacation = { key: 'vacation', color: 'red', selectedDotColor: 'blue' };
     const massage = { key: 'massage', color: 'blue', selectedDotColor: 'blue' };
     const workout = { key: 'workout', color: 'green' };
+    var radio_props = [
+      { label: 'Appointments', value: 0 },
+      { label: 'Migraines', value: 1 },
 
+    ];
     var carregar
     if (!this.state.isLoaded) carregar = <View><ActivityIndicator size="large"></ActivityIndicator></View>
+    var tipusView
+    if (this.state.value == 0) {
+      tipusView =
+        <SafeAreaView style={{ flex: 1 }}>
+          <ScrollView style={{ flex: 1 }}>
+            <View style={{ justifyContent: 'center', alignItems: 'center' }}><Text style={{ fontSize: 20 }}>Appointments list</Text></View>
+            {carregar}
+
+            <FlatList
+              data={this.state.llistaCites}
+              renderItem={({ item }) =>
+                <TouchableOpacity onPress={() => this.seeDetailsAppointment(item)}>
+                  <ListItem containerStyle={{ backgroundColor: "#7BF0E6", borderBottomWidth: 1, borderBottomColor: 'white' }}
+                    title={this.transformaData(item)}
+                  />
+                </TouchableOpacity>
+              }
+              ListFooterComponent={this.renderFooter}
+              ItemSeparatorComponent={this.renderSeparator}
+
+              keyExtractor={item => item}
+            />
+          </ScrollView>
+        </SafeAreaView >
+    }
+    else if (this.state.value == 1) {
+      tipusView =
+        <SafeAreaView style={{ flex: 1 }}>
+          <ScrollView style={{ flex: 1 }}>
+            <View style={{ flex: 1 }}>
+              <View style={{ justifyContent: 'center', alignItems: 'center' }}><Text style={{ fontSize: 20 }}>Migraines list</Text></View>
+              {carregar}
+
+              <FlatList
+                data={this.state.llistaMigranyes}
+                renderItem={({ item }) =>
+                  <TouchableOpacity>
+                    <ListItem containerStyle={{ backgroundColor: "#7BF0E6", borderBottomWidth: 1, borderBottomColor: 'white' }}
+                      title={this.transformaData(item)}
+                    />
+                  </TouchableOpacity>
+                }
+                ListFooterComponent={this.renderFooter}
+                ItemSeparatorComponent={this.renderSeparator}
+
+                keyExtractor={item => item}
+              />
+            </View>
+          </ScrollView>
+        </SafeAreaView >
+    }
     return (
 
       <View style={styles.container}>
@@ -169,84 +196,70 @@ export default class prova extends Component {
           centerComponent={{ text: 'Calendar', style: { color: '#fff' } }}
           rightComponent={<Icon name='add' onPress={() => this.afegirCita()} />}
         />
-        <SafeAreaView style={{ flex: 1 }}>
-          <ScrollView style={{ flex: 1 }}>
-            <View>
 
-              {<Calendar
-                // Enable horizontal scrolling, default = false
-                //horizontal={true}
-                // Enable paging on horizontal, default = false
-                //pagingEnabled={true}
-                // Set custom calendarWidth.
-                //calendarWidth={320}
-                // Initially visible month. Default = Date()
-                current={new Date()}
-                // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-                //minDate={'2012-05-10'}
-                // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-                //maxDate={'2012-05-30'}
-                // Handler which gets executed on day press. Default = undefined
-                onDayPress={day => {
-                  //console.log('selected day', day);
-                  this.setState({ isLoaded: false })
-                  this.getMigraines(day.timestamp)
-                  if (this.state.tipus == "Doctor") {
-                    this.getCites(day.timestamp)
-                  }
-                }}
-                // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-                monthFormat={' MMMM yyyy'}
-                // Handler which gets executed when visible month changes in calendar. Default = undefined
-                onMonthChange={month => {
-                  //console.log('month changed', month);
-                }}
-                // Hide month navigation arrows. Default = false
-                //hideArrows={true}
-                // Do not show days of other months in month page. Default = false
-                //hideExtraDays={true}
-                // If hideArrows=false and hideExtraDays=false do not swich month when tapping on greyed out
-                // day from another month that is visible in calendar page. Default = false
-                disableMonthChange={true}
-                // If firstDay=1 week starts from Monday. Note that dayNames and dayNasmesShort should still start from Sunday.
-                firstDay={1}
+        <View>
 
-                markedDates={{
-                  '2019-11-15': { dots: [vacation, massage, workout], },
-                  '2019-11-17': { marked: true },
-                  '2019-11-16': { marked: true, dotColor: 'red', activeOpacity: 0 },
-                }}
-                markingType={'multi-dot'}
+          {<Calendar
+            // Enable horizontal scrolling, default = false
+            //horizontal={true}
+            // Enable paging on horizontal, default = false
+            //pagingEnabled={true}
+            // Set custom calendarWidth.
+            //calendarWidth={320}
+            // Initially visible month. Default = Date()
+            current={new Date()}
+            // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+            //minDate={'2012-05-10'}
+            // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+            //maxDate={'2012-05-30'}
+            // Handler which gets executed on day press. Default = undefined
+            onDayPress={day => {
+              //console.log('selected day', day);
+              this.setState({ isLoaded: false })
+              this.getMigraines(day.timestamp)
+              if (this.state.tipus == "Doctor") {
+                this.getCites(day.timestamp)
+              }
+            }}
+            // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+            monthFormat={' MMMM yyyy'}
+            // Handler which gets executed when visible month changes in calendar. Default = undefined
+            onMonthChange={month => {
+              //console.log('month changed', month);
+            }}
+            // Hide month navigation arrows. Default = false
+            //hideArrows={true}
+            // Do not show days of other months in month page. Default = false
+            //hideExtraDays={true}
+            // If hideArrows=false and hideExtraDays=false do not swich month when tapping on greyed out
+            // day from another month that is visible in calendar page. Default = false
+            disableMonthChange={true}
+            // If firstDay=1 week starts from Monday. Note that dayNames and dayNasmesShort should still start from Sunday.
+            firstDay={1}
 
-              />}
-            </View>
-            <Swipeable
-              renderRightActions={this.renderRightComponent}
-              >
-              
-              <View>
-                <View style={{ justifyContent: 'center', alignItems: 'center' }}><Text style={{ fontSize: 20 }}>Appointments list</Text></View>
-                {carregar}
+            markedDates={{
+              '2019-11-15': { dots: [vacation, massage, workout], },
+              '2019-11-17': { marked: true },
+              '2019-11-16': { marked: true, dotColor: 'red', activeOpacity: 0 },
+            }}
+            markingType={'multi-dot'}
 
-                <FlatList
-                  data={this.state.llistaCites}
-                  renderItem={({ item }) =>
-                    <TouchableOpacity onPress={() => this.seeDetailsAppointment(item)}>
-                      <ListItem containerStyle={{ backgroundColor: "#7BF0E6", borderBottomWidth: 1, borderBottomColor: 'white' }}
-                        title={this.transformaData(item)}
-                      />
-                    </TouchableOpacity>
-                  }
-                  ListFooterComponent={this.renderFooter}
-                  ItemSeparatorComponent={this.renderSeparator}
+          />}
+        </View>
+        <View style={{ flex: 1}}>
+          <RadioForm
+            radio_props={radio_props}
+            initial={0}
+            formHorizontal={true}
+            onPress={(value) => { this.setState({ value: value }) }}
+            style={{ justifyContent: 'space-around' }}
+          />
 
-                  keyExtractor={item => item}
-                />
-              </View>
-            </Swipeable>
-          </ScrollView>
-        </SafeAreaView>
 
+          {tipusView}
+
+
+        </View>
       </View>
 
     );
